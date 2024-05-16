5bf84981d109171cdf0293d2023eff1f110f23e6
thanuwipro <thanujakb1@gmail.com>
2024-05-16 20:03:05 +0530

    Update File1.java Added 2nd line of code
    added

--- File1.java
+++ File1.java
@@ -1,1 +1,9 @@
-good morning
+import java.lang.*;
+
+public class File1
+  {
+    public static void main(String[] args){
+      System.out.println("first java");
+      System.out.println("my second line of code");
+    }
+  }
