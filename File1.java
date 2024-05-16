2dae463b78a4f350dcf4bb76cbb010f742c5ce8b
thanuwipro <thanujakb1@gmail.com>
2024-05-16 20:07:24 +0530

    Update File1.java variable x added

--- File1.java
+++ File1.java
@@ -2,8 +2,10 @@ import java.lang.*;
 
 public class File1
   {
+    int x=1000;
     public static void main(String[] args){
       System.out.println("first java");
       System.out.println("my second line of code");
+      
     }
   }
