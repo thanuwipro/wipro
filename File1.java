ebe7ec81e7d5727889ec8b47e08875d540a3e6e4
thanuwipro <90082878+Thanujakb@users.noreply.github.com>
2024-05-17 09:52:35 +0530

    add() Added

--- File1.java
+++ File1.java
@@ -1,11 +1,17 @@
 import java.lang.*;
 
 public class File1
-  {
+  
+{
     int x=1000;
+    int y=200;
+
+    public void add() {
+	    System.out.println(x+y);
     public static void main(String[] args){
       System.out.println("first java");
       System.out.println("my second line of code");
       
     }
   }
+  
